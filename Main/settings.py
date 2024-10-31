eca88828399b672c96d1faa995ea9f06dda80212
CpedaMonsterEnergyLover <v.yagnaev@team.globus-ltd.com>
2024-10-31 13:10:22 +0300

    change static suitable for pythonanywhere com v2

--- Main/settings.py
+++ Main/settings.py
@@ -99,8 +99,8 @@ USE_I18N = True
 
 USE_TZ = False
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'assets'
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
