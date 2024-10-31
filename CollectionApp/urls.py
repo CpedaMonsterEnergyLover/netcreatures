50be758d8a1be75e7db6aa9a718ab6f2bf08c6ef
CpedaMonsterEnergyLover <v.yagnaev@team.globus-ltd.com>
2024-10-31 13:32:52 +0300

    encounter api

--- CollectionApp/urls.py
+++ CollectionApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import GetEncounter, FinishEncounter
+from .views import get_encounter, finish_encounter
 
 urlpatterns = [
-    path('encounter/get/', GetEncounter.as_view(), name="get-encounter"),
-    path('encounter/finish/', FinishEncounter.as_view(), name="finish-encounter"),
+    path('encounter/get/', get_encounter, name="get-encounter"),
+    path('encounter/finish/', finish_encounter, name="finish-encounter"),
 ]
