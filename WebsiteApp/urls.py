46a25820d1b79003188b1b50dca5bef35d7569f8
CpedaMonsterEnergyLover <v.yagnaev@team.globus-ltd.com>
2024-11-02 14:56:11 +0300

    fix

--- WebsiteApp/urls.py
+++ WebsiteApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from views import GetWebsiteSignature
+from .views import GetWebsiteSignature
 
 urlpatterns = [
     path('websites/get_signature/', GetWebsiteSignature.as_view(), name="get-signature"),
